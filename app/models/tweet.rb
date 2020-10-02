2a8ad7c778d7e5786e33514e3dc24ab78006ba84
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-02 20:03:52 +0900

    テキストに加え、画像も空では投稿できないように、バリデーションを追記します。

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  validates :text, :image, presence: true
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
 end
